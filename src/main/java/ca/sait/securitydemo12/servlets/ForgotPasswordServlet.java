7c9695e39bcf65a33eb9d75cb18b5e55b4f05bb0
Lisa Jowett <lisajowett@live.ca>
2022-04-07 19:32:31 -0600

    Created forgot.jsp, resetpassword.html, ForgotPasswordServlet.java

---
+++ src/main/java/ca/sait/securitydemo12/servlets/ForgotPasswordServlet.java
@@ -0,0 +1,48 @@
+package ca.sait.securitydemo12.servlets;
+
+import ca.sait.securitydemo12.services.AccountService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Lisa Jowett
+ */
+public class ForgotPasswordServlet extends HttpServlet 
+{
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);    
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        
+        AccountService as = new AccountService();
+    }
+}
